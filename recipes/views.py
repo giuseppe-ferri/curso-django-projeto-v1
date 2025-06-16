b87c34336e4671480fb0b2a729a4f9b566dc484c
Giuseppe Ferri <gferri.dev@gmail.com>
2025-06-16 16:13:32 -0300

    add Faker framework to generate random values to test

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 # Django reconize the templates folders and search the HTML into it
 # Add name spacing "recipes" into the templates folder to avoid name colision
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)]
+    })
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html')
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+    })
