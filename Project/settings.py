d7e1a1bb183b8a1a04a0ebda2ccb59b5c64650fc
Giuseppe Ferri <gferri.dev@gmail.com>
2025-11-03 19:18:17 -0300

    pytest

--- Project/settings.py
+++ Project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-et%1)!p_+mc@x4c%$m6)tzezq!kgoyc@8nbwpsu((=l&pjk+*!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: list[str] = []
 
 
 # Application definition
